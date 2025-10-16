bb822ee66aed70d334e8403441ab27e82479c26c
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-10-16 10:06:33 +0200

    feat: add router directory with app routing setup

---
+++ Movies/src/router/Router.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+      },
+    ],
+  },
+]);
