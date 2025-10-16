2c7c32dc79a3948cc8180589e89d12a97508ea00
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-10-16 10:01:55 +0200

    chore: setup root entry point with RouterProvider integration

--- Movies/src/main.tsx
+++ Movies/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+// @ts-expect-error: Module has no declaration file (Router is a JS file)
+import Router from "./router/Router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={Router} />
+  </React.StrictMode>
+);
