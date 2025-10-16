67d883a1ee5d06ef5bf4572d972e9bc4fd88aee1
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-10-16 16:41:59 +0200

    update: updated main.tsx file

--- Movies/src/main.tsx
+++ Movies/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 // @ts-expect-error: Module has no declaration file (Router is a JS file)
-import Router from "./router/Router";
+import { Router } from "./router/Router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
