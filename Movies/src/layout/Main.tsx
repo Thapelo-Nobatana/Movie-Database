59524188ce68ef65619293f590dcb86a2d14094f
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-10-16 15:01:00 +0200

    update: updated Main.tsx file

--- Movies/src/layout/Main.tsx
+++ Movies/src/layout/Main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
-
+import "../index.css";
 const Main: React.FC = () => {
   return (
     <>
