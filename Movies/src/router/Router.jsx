ae346254e0ea28b44c760c51bb10043b3e06b542
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-10-16 16:47:12 +0200

    fix: fixed router/Router

--- Movies/src/router/Router.jsx
+++ Movies/src/router/Router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 
-const Router = createBrowserRouter([
+export const Router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
