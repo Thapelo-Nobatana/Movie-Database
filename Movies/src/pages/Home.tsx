d550d0e169269b0b9d44ba131be2ed8ef77f113d
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-10-18 12:25:32 +0200

    feat(movie): create MoviesCard component and connect API logic to display fetched movie data

---
+++ Movies/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../components/sections/Hero";
+import Movies from "../components/sections/Movies";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Hero />
+      <Movies />
+    </>
+  );
+};
+
+export default Home;
